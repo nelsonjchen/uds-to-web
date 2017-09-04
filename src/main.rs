6a27b0cd9672554f7f9aab7923ceffd91d10c929
Nelson Chen <crazysim@gmail.com>
2017-09-04 01:51:43 -0700

    Oh, it works!

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,12 @@
 //! connected clients they'll all join the same room and see everyone else's
 //! messages.
 
+extern crate bytes;
 extern crate futures;
 extern crate tokio_core;
+extern crate tokio_io;
 extern crate tokio_tungstenite;
+extern crate tokio_uds;
 extern crate tungstenite;
 
 use std::cell::RefCell;
@@ -28,13 +31,54 @@ use std::env;
 use std::io::{Error, ErrorKind};
 use std::rc::Rc;
 
+use bytes::BytesMut;
 use futures::stream::Stream;
 use futures::Future;
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
+use tokio_io::codec::{Decoder, Encoder};
 use tungstenite::protocol::Message;
 
 use tokio_tungstenite::accept_async;
+
+pub struct LineCodec;
+
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = std::io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> std::io::Result<Option<String>> {
+        if let Some(i) = buf.iter().position(|&b| b == b'\n') {
+            // remove the serialized frame from the buffer.
+            let line = buf.split_to(i);
+
+            // Also remove the '\n'
+            buf.split_to(1);
+
+            // Turn this data into a UTF string and return it in a Frame.
+            match std::str::from_utf8(&line) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) => Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "invalid UTF-8",
+                )),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for LineCodec {
+    type Item = String;
+    type Error = std::io::Error;
+
+    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> std::io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\n");
+        Ok(())
+    }
+}
 
 fn main() {
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
@@ -50,7 +94,7 @@ fn main() {
     // store the map of all connections we know about.
     let connections = Rc::new(RefCell::new(HashMap::new()));
 
-    let srv = socket.incoming().for_each(|(stream, addr)| {
+    let ws_srv = socket.incoming().for_each(|(stream, addr)| {
         // We have to clone both of these values, because the `and_then`
         // function billow constructs a new future, `and_then` requires
         // `FnOnce`, so we construct a move closure to move the
@@ -122,10 +166,45 @@ fn main() {
             })
     });
 
-    let stupid_future = futures::future::ok::<(), std::io::Error>(());
 
-    let mixed_srv = stupid_future.join(srv);
 
+    let uds = tokio_uds::UnixListener::bind("/tmp/uds", &handle).unwrap();
+    let uds_srv = uds.incoming().for_each(|(stream, addr)| {
+        // We have to clone both of these values, because the `and_then`
+        // function billow constructs a new future, `and_then` requires
+        // `FnOnce`, so we construct a move closure to move the
+        // environment inside the future (AndThen future may overlive our
+        // `for_each` future).
+        let connections_inner = connections.clone();
+        let handle_inner = handle.clone();
+        let addr_inner = addr.clone();
+
+        use tokio_io::AsyncRead;
+        let (sink, stream) = stream.framed(LineCodec).split();
+
+        let uds_reader = stream.for_each(move |line: String| {
+            println!("Received a UDS message: {}", line);
+
+            // For each open connection except the sender, send the
+            // string via the channel.
+            let mut conns = connections_inner.borrow_mut();
+            let iter = conns.iter_mut().map(|(_, v)| v);
+            for tx in iter {
+                tx.unbounded_send(tungstenite::Message::Text(line.clone())).unwrap();
+            }
+            Ok(())
+        });
+
+        let connection = uds_reader
+                    .map(|_| ())
+                    .map_err(|_| ());
+
+        handle_inner.spawn(connection);
+
+        Ok(())
+    });
+
+    let mixed_srv = uds_srv.join(ws_srv);
 
     // Execute server.
     core.run(mixed_srv).unwrap();
